e565e3a7d6fd1d3980005eb665878494b9c1ad54
Bonnie Pickett <bonniepickett@Bonnies-MacBook-Pro.local>
2024-08-06 19:53:56 -0500

    updated projectMeta.js with correct data

--- build/1-project-bundle/sampleProjectMeta.js
+++ build/1-project-bundle/sampleProjectMeta.js
@@ -1,21 +1,20 @@
 export const projectInfo = {
-    projectName: "Phi Geometry",
-    artistsPayoutWallet: "0x9fcb72D67A4fE972FBEae178647f61A9EC3fF8f3",
-    numberOfEditions: 2,
-    releaseDate: "2024-02-01 09:00:00",
-    websiteProjectDescription:
-      "Three intersecting planes, all in the proportions of the Golden Ratio (Phi), rotate around a common axis.  The background color and color of each plane are determined by each token's unique hash.",
-    price: 0.01,
-    royaltiesPercent: 7.5,
-    titleForViewport: "A Phi-based Shape in THREEjs",
-    projectWebUrl: "https://mact6340-app-nzv3s.ondigitalocean.app/projects",
-    openSeaCollectionName: "Phi Geometry",
-    openSeaCollectionDescription:
-      "A Phi-based interactive shape rendered with THREEjs.",
-    openSeaCollectionImage: "",
-    openSeaCollectionSeller_fee_basis_points: 750,
-    openSeaCollectionFee_recipient: "0x9fcb72D67A4fE972FBEae178647f61A9EC3fF8f3",
-    tokenDescriptionText:
-      "One of the gorgeous interactive shapes from the 'Phi Geometry' collection.",
-  };
-  
+  projectName: "Icosahedron Geometry",
+  artistsPayoutWallet: "0x94848CEe6eA7dBcc5322f0B13015A42ec63bC3BB",
+  numberOfEditions: 13,
+  releaseDate: "2024-02-01 21:00:00",
+  websiteProjectDescription:
+    "An Icosahedron that rotates around an axis.  The background color and color of the knot are determined by each unique hash.",
+  price: 0.01,
+  royaltiesPercent: 5.0,
+  titleForViewport: "An Icosahedron in THREEjs",
+  projectWebUrl: "https://mact6340-app-nzv3s.ondigitalocean.app/projects",
+  openSeaCollectionName: "Icosahedron Geometry",
+  openSeaCollectionDescription:
+    "An Icosahedron interactive shape rendered with THREEjs.",
+  openSeaCollectionImage: "",
+  openSeaCollectionSeller_fee_basis_points: 500,
+  openSeaCollectionFee_recipient: "0x94848CEe6eA7dBcc5322f0B13015A42ec63bC3BB",
+  tokenDescriptionText:
+    "One of the gorgeous interactive shapes from the Icosahedron collection.",
+};
